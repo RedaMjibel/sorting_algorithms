67949fb7e9ecbbf3bf813fa5e4cd750c3c4903b5
redamjibel <redamjibel@hotmail.com>
2023-09-25 19:19:13 -0700

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,7 +21,7 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[k])
 				k = j;
 		}
-		if (i > k)
+		if (array[i] > array[k])
 		{
 			temp = array[i];
 			array[i] = array[k];
