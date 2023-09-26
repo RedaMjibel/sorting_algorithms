bea531896ebca19cd137d11a873640af3b026af5
redamjibel <redamjibel@hotmail.com>
2023-09-25 19:21:04 -0700

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * selectio_sort -  sorts an array of integers in ascending order
+ * selection_sort -  sorts an array of integers in ascending order
  * @array: array to be sorted
  * @size: size of the array
  *
@@ -14,9 +14,9 @@ void selection_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size -1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for(j = size - 1, k = i + 1; j > i; j--)
+		for (j = size - 1, k = i + 1; j > i; j--)
 		{
 			if (array[j] < array[k])
 				k = j;
